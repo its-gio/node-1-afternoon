f428d60ad8dc5ee7dcf5004c7a3e98f66342705f
Giovani Aguirre <me@its-g.io>
2020-01-24 14:36:48 -0600

    Endpoint made for products API to be deisplayed

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const products = require('../products.json')
+
+app.get("/api/products", (req, res) => res.status(200).json(products))
 
 app.listen(2020, () => console.log("Working!"));
