3b9332812d1b6320b38ac37cf4fac48e4c2c5d7a
Giovani Aguirre <me@its-g.io>
2020-01-24 15:58:47 -0600

    Added query for price to products search

--- server/getProducts.js
+++ server/getProducts.js
@@ -1,11 +1,19 @@
 const products = require('../products.json')
 
-const displayProducts = (req, res) => res.status(200).json(products);
+const displayProducts = (req, res) => {
+  // console.log(req.query)
+  if (req.query.price) {
+    let results = products.filter(item => item.price === +req.query.price)
+    return res.status(200).json(results);
+  }
+  return res.status(200).json(products);
+}
+
 const displayProduct = (req, res) => {
   let result = products.filter(item => item.id === +req.params.idNum);
 
   if (result.length) {
-    res.status(200).json(result)
+    res.status(200).json(result);
   } else {
     res.status(404).send(`<h1>Item #${req.params.idNum} does not exist!</h1>`)
   }
--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const { displayProducts, displayProduct } = require('./getProducts')
 const app = express();
 
-app.get("/api/products", displayProducts)
-app.get(`/api/products/:idNum`, displayProduct)
+app.get(`/api/products/:idNum`, displayProduct);
+app.get("/api/products", displayProducts);
 
 
-app.listen(2020, () => console.log("Working!"));
+app.listen(2020, () => {});
