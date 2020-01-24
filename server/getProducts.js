46bee79ec9718f4be8a2459eb05119e505383d24
Giovani Aguirre <me@its-g.io>
2020-01-24 15:08:11 -0600

    Added endpoint for single product to be shown

--- server/getProducts.js
+++ server/getProducts.js
@@ -1,9 +1,17 @@
 const products = require('../products.json')
 
-function displayProducts(req, res) {
-  res.status(200).json(products)
+const displayProducts = (req, res) => res.status(200).json(products);
+const displayProduct = (req, res) => {
+  let result = products.filter(item => item.id === +req.params.idNum);
+
+  if (result.length) {
+    res.status(200).json(result)
+  } else {
+    res.status(404).send(`<h1>Item #${req.params.idNum} does not exist!</h1>`)
+  }
 }
 
 module.exports = {
-  displayProducts
+  displayProducts,
+  displayProduct
 }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { displayProducts } = require('./getProducts')
+const { displayProducts, displayProduct } = require('./getProducts')
 const app = express();
 
 app.get("/api/products", displayProducts)
+app.get(`/api/products/:idNum`, displayProduct)
 
 
 app.listen(2020, () => console.log("Working!"));
